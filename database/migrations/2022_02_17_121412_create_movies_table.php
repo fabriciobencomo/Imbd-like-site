4e5dadc2b197010d6f1e0da185f10715c0c5a46c
Fabricio Bencomo <Fabriciobencomo.viloria@gmail.com>
2022-02-17 08:48:20 -0400

    imdb-like-site config ready

---
+++ database/migrations/2022_02_17_121412_create_movies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->integer('duration');
+            $table->string('cast');
+            $table->integer('year');
+            $table->foreignId('category_id')->constrained();
+            $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+};
---
+++ tests/Unit/MovieTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Movie;
+use App\Models\Category;
+use PHPUnit\Framework\TestCase;
+
+class MovieTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+}
